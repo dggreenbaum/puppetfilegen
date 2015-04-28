fd7208ed1d4d5456a4048f0d9870abf477522b98
Douglas Greenbaum <dggreenbaum@greenbad.org>
2015-04-27 19:09:45 -0700

    Remove some clutter.

--- puppetfilegen.rb
+++ puppetfilegen.rb
@@ -17,8 +17,6 @@ end.parse!
 puts "forge 'forge.puppetlabs.com'"
 Dir.glob("#{puppet_env}/modules/*/metadata.json") do |item|
   next if item == '.' or item == '..'
-  # do work on real items
   data = JSON.parse(File.read(item))
-  name =
   puts "mod '#{data['name'].gsub!('-', '/')}', '#{data['version']}'"
 end
